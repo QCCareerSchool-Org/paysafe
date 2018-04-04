58620d2e52dbfa487f464e0b292fd37edb01b80d
Dave Welsh <dave@qccareerschool.com>
2018-04-04 12:41:09 -0400

    version 0.0.7

--- dist/environment.js
+++ dist/environment.js
@@ -9,6 +9,6 @@ class Environment {
 }
 exports.Environment = Environment;
 const MAX_SOCKETS = 10;
-const TIMEOUT = 600; // 60 seconds
+const TIMEOUT = 60000; // 60 seconds
 exports.LIVE = new Environment('https://api.paysafe.com', MAX_SOCKETS, TIMEOUT);
 exports.TEST = new Environment('https://api.test.paysafe.com', MAX_SOCKETS, TIMEOUT);
